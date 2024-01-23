752c48507681a2b631c4ec63e681af5699c09f26
kwonhygge <cleancode.kwon@gmail.com>
2024-01-23 23:42:01 +0900

    first commit

---
+++ packages/db/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'aryc6cw4',
+    dataset: 'production'
+  }
+})
---
+++ packages/db/sanity.config.ts
@@ -0,0 +1,23 @@
+import {defineConfig} from 'sanity'
+import {structureTool} from 'sanity/structure'
+import {visionTool} from '@sanity/vision'
+//import {googleMapsInput} from '@sanity/google-maps-input'
+import {schemaTypes} from './schemas'
+
+export default defineConfig({
+  name: 'default',
+  title: 'db',
+
+  projectId: 'aryc6cw4',
+  dataset: 'production',
+
+  plugins: [
+    structureTool(),
+    visionTool(),
+    //googleMapsInput(),
+  ],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
