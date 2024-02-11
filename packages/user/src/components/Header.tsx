fdf2407523c192462e401d47f22076e1271db4fe
kwonhygge <cleancode.kwon@gmail.com>
2024-02-12 00:09:12 +0900

    feat: header 반응형 css 추가

--- packages/user/src/components/Header.tsx
+++ packages/user/src/components/Header.tsx
@@ -3,10 +3,10 @@ export default function Header() {
     return (
         <header className={`${manrope.className} absolute top-7.5 w-full`}>
             <nav className="mx-auto flex w-full max-w-[1200px] items-center justify-between px-4 md:px-7.5 lg:px-0">
-                <h1 className="text-3xl font-bold">
+                <h1 className="text-xl font-bold lg:text-3xl">
                     <a href="/">hee</a>
                 </h1>
-                <ul className="flex items-center gap-x-13.5">
+                <ul className="flex items-center gap-x-13.5 text-sm lg:text-base">
                     <li>
                         <a href="/list">Works</a>
                     </li>
