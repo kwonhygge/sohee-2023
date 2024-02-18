12e8467fa905ed3138219cbbc93f865c8cb831f6
kwonhygge <cleancode.kwon@gmail.com>
2024-02-18 23:57:57 +0900

    feat: info page 마무리
    - WorkHistory 컴포넌트 마무리

--- packages/user/src/app/info/page.tsx
+++ packages/user/src/app/info/page.tsx
@@ -8,7 +8,7 @@ export default async function Page() {
     const myInfoData = await getMyInfo<MyInfoData>()
 
     return (
-        <div className="mx-auto flex max-w-[1200px] flex-col items-center gap-y-20 px-7.5 pt-24 lg:flex-row lg:items-start lg:justify-between lg:gap-y-0 lg:px-0 lg:pt-52">
+        <div className="mx-auto flex max-w-[1200px] flex-col items-center gap-y-20 px-7.5 pb-20 pt-24 lg:flex-row lg:items-start lg:justify-between lg:gap-y-0 lg:px-0 lg:pb-48 lg:pt-52">
             <MyInfo data={myInfoData} />
             <div className="grid gap-y-20 lg:gap-y-25">
                 <WorkProcess />
