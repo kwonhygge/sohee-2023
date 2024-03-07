a484e6441da1e7ab335b1ddde98d318b14fbeb3c
kwonhygge <cleancode.kwon@gmail.com>
2024-03-07 12:08:10 +0900

    fix: google tag 추가방식 수정

--- packages/user/src/app/layout.tsx
+++ packages/user/src/app/layout.tsx
@@ -2,8 +2,7 @@ import type { Metadata } from 'next'
 import { Noto_Sans_KR } from 'next/font/google'
 import './globals.scss'
 import Header from '@/components/Header'
-import { Suspense } from 'react'
-import { Analytics } from '@/lib/Analytics'
+import Script from 'next/script'
 
 const notoSansKR = Noto_Sans_KR({ subsets: ['latin'] })
 
@@ -19,12 +18,23 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="ko">
+            <Script id="google-tag-manager" strategy="afterInteractive">
+                {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','${process.env.GTM_ID}');
+        `}
+            </Script>
             <body className={notoSansKR.className}>
-                <Suspense>
-                    <Analytics />
-                </Suspense>
                 <Header />
                 <main>{children}</main>
+                <noscript
+                    dangerouslySetInnerHTML={{
+                        __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${process.env.GTM_ID}" height="0" width="0" style="display: none; visibility: hidden;"></iframe>`,
+                    }}
+                />
             </body>
         </html>
     )
